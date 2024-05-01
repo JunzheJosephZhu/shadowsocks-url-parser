5e34efcd09385f589cf8392367fb9e7d1554aeaf
JunzheJosephZhu <junzhe.joseph.zhu@gmail.com>
2024-05-01 22:11:14 +0800

    clean up extra configs

--- parse-shadowsocks.py
+++ parse-shadowsocks.py
@@ -14,6 +14,7 @@ def create_config_file(method, password, server, port, config_id):
     config = {
         "server": server,
         "server_port": int(port),
+        "local_address": "0.0.0.0",
         "local_port": 1080,
         "password": password,
         "timeout": 300,
