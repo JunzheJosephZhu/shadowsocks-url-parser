4355ad026c31d5aa75e41cfa29cd050a2cbfaf8f
JunzheJosephZhu <junzhe.joseph.zhu@gmail.com>
2024-07-29 03:26:25 +0800

    change taiwan node

--- clash_configs/start_clash.sh
+++ clash_configs/start_clash.sh
@@ -1,6 +1,10 @@
-# mkdir -p ~/.clash
-# ln -s ./clash_configs/config.yaml ~/.clash/config.yaml
-# ln -s ./clash_configs/gcp.yaml ~/.clash/gcp.yaml
-# ln -s ./clash_configs/Country.mmdb ~/.clash/Country.mmdb
+# add the following to .bashrc
+# export http_proxy=http://127.0.0.1:7890/
+# export HTTP_PROXY=http://127.0.0.1:7890/
+ # this has to be http, not https
+# export https_proxy=http://127.0.0.1:7890/
+# export HTTPS_PROXY=http://127.0.0.1:7890/
+# export all_proxy=socks5://127.0.0.1:7891/
+# export ALL_PROXY=socks5://127.0.0.1:7891/
 chmod +x ./clash-linux-amd64-v3
 ./clash-linux-amd64-v3 -f clash_configs/gcp.yml
