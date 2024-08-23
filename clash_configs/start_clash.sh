94d99fb2638243d122ebff715cb102665296947f
SheldonTsui <forrest.tusi@gmail.com>
2024-08-23 15:35:16 +0800

    change to gcp config

--- clash_configs/start_clash.sh
+++ clash_configs/start_clash.sh
@@ -7,4 +7,4 @@
 # export all_proxy=socks5://127.0.0.1:7891/
 # export ALL_PROXY=socks5://127.0.0.1:7891/
 chmod +x ./clash-linux-amd64-v3
-./clash-linux-amd64-v3 -f clash_configs/gcp.yml
+./clash-linux-amd64-v3 -f clash_configs/baiyueguang.yml
